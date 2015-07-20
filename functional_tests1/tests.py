f7253b77afa11c9e6c7af5b4c56e9a0305bba5ed
BorzdovBogdan <bodja7.borzdov@yandex.ru>
2015-07-20 14:24:45 +0300

    New test class. Also new URL, view and template to display lists

--- functional_tests1/tests.py
+++ functional_tests1/tests.py
@@ -49,18 +49,15 @@ class NewVisitorTest(LiveServerTestCase):
 
         #self.fail('Finish the test!')
 
-        self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
-        self.check_for_row_in_list_table('1: Buy peacock feathers')
-
         self.browser.quit()
         self.browser = webdriver.Firefox()
 
         self.browser.get(self.live_server_url)
-        page_text = self.browser.find_elements_by_tag_name('body').text
+        page_text = self.browser.find_element_by_tag_name('body').text
         self.assertNotIn('Buy peacock feathers', page_text)
         self.assertNotIn('make a fly', page_text)
 
-        inputbox = self.browser.find_elements_by_id('id new item')
+        inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Buy milk')
         inputbox.send_keys(Keys.ENTER)
 
@@ -68,7 +65,7 @@ class NewVisitorTest(LiveServerTestCase):
         self.assertRegexpMatches(francis_list_url, '/lists/.+')
         self.assertNotEqual(francis_list_url, edith_list_url)
 
-        page_text = self.browser.find_elements_by_tag_name('body').text
+        page_text = self.browser.find_element_by_tag_name('body').text
         self.assertNotIn('Buy peacock feathers', page_text)
         self.assertIn('Buy milk', page_text)
 
